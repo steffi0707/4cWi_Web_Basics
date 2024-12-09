d2f91a175be5e3dda57f183c57d20e67c6a7dde1
Steffi <Steffi@htldornbirn.at>
2024-12-09 13:35:33 +0100

    finished_02_Strings_und_Zahlen

--- HTML,CSS,JS/01_Variablen_und_Operatoren/script.js
+++ HTML,CSS,JS/01_Variablen_und_Operatoren/script.js
@@ -1,1 +1,28 @@
 console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+/*let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert" + a );
+
+a = 10;
+console.log("A hat den Wert" + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+// *, /, +, - 
+
+c = b - a;
+console.log("b - a = " + c);
+*/
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result); 
---
+++ HTML,CSS,JS/02_Strings_und_Zahlen/script.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+let c = a / b;
+
+console.log("The result of 17.4 devided by 3.9 is " + c);
+
+let firstname = "Stefanie";
+let lastname = "Trautz";
+
+console.log("Ich heiße " + firstname +" "+ lastname);
