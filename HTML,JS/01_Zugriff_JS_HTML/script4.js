cc4cfa6359f75d8e7d3bc3bd2bab4248d84a7016
Steffi <Steffi@htldornbirn.at>
2025-02-07 10:01:23 +0100

    finished 01_Zugriff_JS_HTML

--- HTML,JS/01_Zugriff_JS_HTML/script4.js
+++ HTML,JS/01_Zugriff_JS_HTML/script4.js
@@ -1,10 +1,31 @@
-
-function buttonClicked(){
-    console.log("clicked");
-    let num1 = document.getElementById("number1").value;
-    let num2 = document.getElementById("number2").value;
-
+function addition(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
     let result = parseFloat(num1) + parseFloat(num2);
 
-    console.log(result);
+    printResult.innerHTML = result;
+}
+
+function substract(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) - parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
+}
+
+function multiply(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) * parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
+}
+
+function divide(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let result = parseFloat(num1) / parseFloat(num2);
+
+    printResult.innerHTML = "Result = " + result  ;
 }
