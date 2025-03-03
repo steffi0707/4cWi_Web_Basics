ba73c3767fc6b16943c53a36d53339e4779ec721
Steffi <Steffi@htldornbirn.at>
2025-03-03 15:31:08 +0100

    finished API_Football

--- JS_Ajax/02_Football-API/script.js
+++ JS_Ajax/02_Football-API/script.js
@@ -5,7 +5,7 @@ function loadFootballTable(){
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        let html = "<div>";
+        let html = "<div id='table'>";
 
         for (let i = 0; i < json.length; i++){
             html += `<div data-teamId="${json[i].teamInfoId}">${json[i].teamName}</div>`;
@@ -13,4 +13,27 @@ function loadFootballTable(){
         html += "</div>";
         document.getElementById("table").innerHTML = html;
       });
+}
+
+document.getElementById("table").addEventListener("click", (event) => {
+  let selectedTeamId = event.target.getAttribute("data-teamId");
+  console.log("selectedTeamId", selectedTeamId);
+  getNextMatchForTeam(selectedTeamId);
+})
+
+
+
+function getNextMatchForTeam(teamId){
+  fetch(`https://api.openligadb.de/getnextmatchbyleagueteam/4741/${teamId}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+
+      let html = "div id='nextMatch'>";
+      html += `<div>${json.matchDateTime}</div>`;
+
+      html += `<div><img width = "40px"; src="${json.team1.teamIconUrl}"/>${json.team1.teamName} vs. ${json.team2.teamName}<img width = "40px"; src="${json.team2.teamIconUrl}"/></div>`;
+      html += "</div>";
+      document.getElementById("nextMatch").innerHTML = html;
+    })
 }
