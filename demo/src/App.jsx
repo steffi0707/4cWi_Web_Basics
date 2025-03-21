f76cdc87cb98d72449f506a0c2d12bc059ef9388
Steffi <Steffi@htldornbirn.at>
2025-03-21 09:27:39 +0100

    finished_1_Setup_React_&_Tailwind_&_erste_Komponente

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='bg-amber-300 border'>Hello</h1>
-      <Card />
+      <h1 className="bg-green-400 p-4 text-center">Hello</h1>
+      <div className="flex">
+      <Card name={"Lenz"} jobtitle={"AV"} description={"lässig"} />
+      <Card name={"Sams"} jobtitle={"AV"} description={"auch lässig"} />
+      </div>
       test
     </>
   );
--- demo/src/components/Card.jsx
+++ demo/src/components/Card.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export default function Card() {
-    return <div className='bg-orange-200 border'>Card</div>;
+export default function Card({name, jobtitle, description}) {
+    return( 
+        <div className="bg-orange-200 border w-full">
+            <p className="font-bold">{name}</p>
+            <p>{jobtitle}</p>
+            <p>{description}</p>
+        </div>
+    );
 }
